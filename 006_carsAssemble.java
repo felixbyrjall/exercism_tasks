27631860c5541a48925b2f595f6a9f5059a251fd
felixbyrjall <113479176+felixbyrjall@users.noreply.github.com>
2023-09-19 13:16:04 +0200

    Solution for the CarsAssemble task

--- 006_carsAssemble.java
+++ 006_carsAssemble.java
@@ -1,10 +1,23 @@
 public class CarsAssemble {
+    public int carsPerSpeed = 221;
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRateperHour() method");
+        double result;
+        if (speed >= 4) {
+            result = carsPerSpeed * speed;
+        } else if (speed >= 8) {
+            result = (carsPerSpeed * speed) * 0.9;
+        } else if (speed == 9) {
+            result = (carsPerSpeed * speed) * 0.8;
+        } else {
+            result = (carsPerSpeed * speed) * 0.77;
+        }
+        return result;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        double carsPerHours = productionRatePerHour(speed);
+        int carsPerMinute = (int)carsPerHours / 60;
+        return carsPerMinute;
     }
 }
